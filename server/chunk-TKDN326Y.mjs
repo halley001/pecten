a167551c6fed887d9b586b3a766f377914caad3d
halley001 <halleymail100@gmail.com>
2023-11-29 15:09:48 +0100

    Auto-generated commit

--- server/chunk-TKDN326Y.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a as e,b as p,i,k as n,l as m,m as f,p as s,v as a}from"./chunk-42M7YATV.mjs";var c=(()=>{let o=class o{constructor(){this.title="pecten"}};o.\u0275fac=function(r){return new(r||o)},o.\u0275cmp=e({type:o,selectors:[["app-root"]],standalone:!0,features:[p],decls:0,vars:0,template:function(r,x){},dependencies:[n]});let t=o;return t})();var d=[];var u={providers:[a(d),f()]};var g={providers:[s()]},l=i(u,g);var C=()=>m(c,l),q=C;export{q as a};
--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TKDN326Y.mjs";import"./chunk-42M7YATV.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-2URTG24S.mjs";import"./chunk-WX343XRD.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
