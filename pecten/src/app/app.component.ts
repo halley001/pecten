4ef12d33288d65582be281f558c4d68fdf8fc0c0
halley001 <halleymail100@gmail.com>
2023-11-29 16:07:58 +0100

    add: header, styles, assets

--- pecten/src/app/app.component.ts
+++ pecten/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'pecten';
